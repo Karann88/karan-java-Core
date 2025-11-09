8ad60cd49285c6a707b3be6686ed892d2ebc1849
Karann88 <karanjangid011@gmail.com>
2025-11-09 20:21:18 +0530

    Add Deque examples

--- STACKSQUEUES/InBuiltExamples.java
+++ STACKSQUEUES/InBuiltExamples.java
@@ -1,4 +1,6 @@
 package STACKSQUEUES;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
@@ -29,5 +31,27 @@ public class InBuiltExamples {
         System.out.println(queue.peek());
         System.out.println(queue.remove());
         System.out.println(queue.remove());
+
+        // Deque: double ended queue that allows insertion and deletion from both ends.
+        // Implemented using LinkedList or ArrayDeque class in Java.
+        // It has not capacity restrictions.
+        // Faster than LinkedList & Stack.
+        // 
+        
+        Deque<Integer> deque = new LinkedList<>();
+        Deque<Integer> deque2 = new ArrayDeque<>();
+        deque.add(1);
+        deque.add(2);
+        deque.add(3);
+        deque.add(4);
+        deque.add(5);
+        deque.addFirst(6);
+        deque.addLast(7);
+        System.out.println(deque);
+        System.out.println(deque.peek());
+        System.out.println(deque.remove());
+        System.out.println(deque.removeFirst());
+        System.out.println(deque.removeLast());
+        System.out.println(deque);
     }
 }
